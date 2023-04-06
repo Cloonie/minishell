b49aacadd1d26ce360af1dc33f4a8bb055c082c8
root <rsdoot@DESKTOP-T5CUUII.localdomain>
2023-04-06 20:59:02 +0800

    working prompt with history, ctrl c/d, pwd

--- main.c
+++ main.c
@@ -10,10 +10,58 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <sys/wait.h>
+#include <dirent.h>
 
-int	main(int argc, char const *argv[])
+#define Ctrl(x) ((x) & 0x1f)
+
+void	ctrl_c(int sig)
 {
-	wait();
+	printf("\n"); // Print a newline to prompt for new input
+	rl_on_new_line(); // Move cursor to the beginning of the line
+	rl_replace_line("", 0); // Clear the current input line
+	rl_redisplay(); // Redisplay the prompt
+}
+
+int	main(void)
+{
+	char	*input;
+	char	cwd[1024];
+	char	display[1024];
+	DIR		*dir;
+	struct dirent	*entry;
+
+	signal(SIGINT, ctrl_c);
+	while (1)
+	{
+		getcwd(display, sizeof(cwd));
+		getcwd(cwd, sizeof(cwd));
+		strcat(display, "> ");
+		input = readline(display);
+		if (input == NULL)
+		{
+			printf("logout\n");
+			break ;
+		}
+		if (strcmp(input, "pwd") == 0)
+			printf("%s\n", cwd);
+		if (strcmp(input, "ls") == 0)
+		{
+			dir = opendir(cwd);
+			while ((entry = readdir(dir)) != NULL)
+				printf("%s\n", entry->d_name);
+			// closedir(dir);
+		}
+		add_history(input);
+		free(input);
+	}
+	rl_clear_history();
+
 	return (0);
 }
