7e2be1575ac75e6a82c4f2f12acdc8a9e8b79fb2
koencheong <koencheong@gmail.com>
2023-06-06 12:01:54 +0800

    Piping demo version

--- piping.c
+++ piping.c
@@ -44,9 +44,13 @@ int	main(int argc, char **argv, char **envp)
 		// Output of child1 should go to the write end of the pipe (instead of STDOUT, i.e. terminal), 
 		// so that the output can be read by child2 through the read end of the pipe.
 		dup2(pipe_fd[1], STDOUT_FILENO);
+
+		// printf("Hi from child1");
+
 		x += 5;
 		// printf("%d\n", x);
 		write(STDOUT_FILENO, &x, sizeof(int));
+		
 		close(pipe_fd[0]);
 		// Do I need this? ⬇️
 		// close(STDIN_FILENO);
@@ -58,6 +62,7 @@ int	main(int argc, char **argv, char **envp)
 	{
 		// Input of child2 should come from the read end of the pipe (which is connected to the output of child1).
 		dup2(pipe_fd[0], STDIN_FILENO);
+
 		// Redirect the output to output file instead of to the terminal.
 		// int output = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		// if (dup2(output, STDOUT_FILENO) == -1)
@@ -65,6 +70,11 @@ int	main(int argc, char **argv, char **envp)
 		// 	perror("Output dup2");
 		// 	exit(1);
 		// }
+
+		// char buffer[20];
+		// read(STDIN_FILENO, &buffer, 20);
+		// printf("Message received: [%s]\n", buffer);
+
 		read(STDIN_FILENO, &x, sizeof(int));
 		printf("%d\n", x += 5);
 		close(pipe_fd[1]);
